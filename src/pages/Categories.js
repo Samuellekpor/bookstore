7e9b58d49c4f661fc67bcd4ba2816afb3b10dd1e
Samuellekpor <samuellekpor@gmail.com>
2022-11-19 02:22:57 +0000

    Fix linter error

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Categories() {
   return (
     <div>
-      <button type='button'>Check status</button>
+      <button type="button">Check status</button>
     </div>
   );
 }
