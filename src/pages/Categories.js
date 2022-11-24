c0f180d3ff66406f4fb7ce97b1ea41f05db9404d
Samuellekpor <samuellekpor@gmail.com>
2022-11-24 01:22:15 +0000

    Add check status functionality

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkBook } from '../redux/categories/categories';
 
 function Categories() {
+  const dispatch = useDispatch();
+
+  function checkBookCategory(){
+    dispatch(checkBook());
+  }
   return (
     <div>
-      <button type="button">Check status</button>
+      <button type="button" onClick={checkBookCategory}>Check status</button>
     </div>
   );
 }
